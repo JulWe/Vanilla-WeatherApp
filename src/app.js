461df82a87b4d3623edef743ecf270ff4a71f01b
JulWe <132477835+JulWe@users.noreply.github.com>
2023-07-10 17:40:03 +0200

    implemented the unit conversion

--- src/app.js
+++ src/app.js
@@ -25,21 +25,24 @@ return `${day} ${hours}:${minutes}`;
 
 function displayTemperature(response) {
 let temperatureElement = document.querySelector("#temperature");
-temperatureElement.innerHTML = Math.round(response.data.temperature.current);
 let cityElement = document.querySelector("#city");
-cityElement.innerHTML = response.data.city;
 let descriptionElement = document.querySelector("#description");
-descriptionElement.innerHTML = response.data.condition.description;
 let humidityElement = document.querySelector("#humidity");
-humidityElement.innerHTML = response.data.temperature.humidity;
 let windElement = document.querySelector("#windspeed");
-windElement.innerHTML = Math.round(response.data.wind.speed);
 let dateElement = document.querySelector("#date");
-dateElement.innerHTML = formatDate(response.data.time * 1000);
 let iconElement = document.querySelector("#icon");
-iconElement.setAttribute("src",`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
 
+celsiusTemperature = response.data.temperature.current;
+
+temperatureElement.innerHTML = Math.round(celsiusTemperature);
+cityElement.innerHTML = response.data.city;
+descriptionElement.innerHTML = response.data.condition.description;
+humidityElement.innerHTML = response.data.temperature.humidity;
+windElement.innerHTML = Math.round(response.data.wind.speed);
+dateElement.innerHTML = formatDate(response.data.time * 1000);
+iconElement.setAttribute("src",`http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`);
 iconElement.setAttribute("alt", response.data.condition.description);
+
 }
 
 function search(city) {
@@ -55,8 +58,35 @@ function handleSubmit(event) {
     search(cityInputElement.value);
 }
 
+function displayFahrenheitTemperature(event) {
+    event.preventDefault();
+    let farhenheitTemperature = (celsiusTemperature * 9) /5 + 32;
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(farhenheitTemperature);
+}
 
-search("New York");
+function displayCelsiusTemperature(event) {
+    event.preventDefault();
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
+    let temperatureElement = document.querySelector("#temperature");
+    temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+let celsiusTemperature = null;
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
+
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
+let celsiusLink = document.querySelector("#celsius-link");
+celsiusLink.addEventListener("click", displayCelsiusTemperature);
+
+
+
+search("Vienna");
